a8b9d80b840f408c13e0c0be01dbd72ce244ca18
HimanshuP90 <himanshup895@gmail.com>
2018-05-30 16:09:53 +0530

    nodejs api with monog

---
+++ app/routes/note_routes.js
@@ -0,0 +1,39 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+
+  app.get('/notes/:id', (req, res) => {
+  	const id = req.params.id
+  	const details = {'_id': new ObjectID(id)};
+  	db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({ 'error': "An error is occured" });
+			} else {
+				res.send(item)
+			}
+		});
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+  	const id = req.params.id
+  	const details = {'_id': new ObjectID(id)};
+  	db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({ 'error': "An error is occured" });
+			} else {
+				res.send('Note ' + id + ' deleted ..!!')
+			}
+		});
+  });
+
+	app.post('/notes', (req, res) => {
+		const note = { text: req.body.body, title: req.body.title}
+		db.collection('notes').insert(note, (err, results) => {
+			if (err) {
+				res.send({ 'error': "An error is occured" });
+			} else {
+				res.send(results.ops[0])
+			}
+		})
+	}); 
+}
